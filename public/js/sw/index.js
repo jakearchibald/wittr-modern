995788cf4d891335a3e29dd8bbce5888249eaae6
Jake Archibald <jaffathecake@gmail.com>
2015-08-28 08:18:08 +0100

    Handling new sw & updates

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -1,6 +1,6 @@
 import './preroll/index.js';
 
-var staticCacheName = 'wittr-static-v2';
+var staticCacheName = 'wittr-static-v3';
 
 self.addEventListener('install', function(event) {
   event.waitUntil(
@@ -40,5 +40,8 @@ self.addEventListener('fetch', function(event) {
   );
 });
 
-// TODO: listen for the "message" event, and call
-// skipWaiting if you get the appropriate message
+self.addEventListener('message', function(event) {
+  if (event.data.action === 'skipWaiting') {
+    self.skipWaiting();
+  }
+});
