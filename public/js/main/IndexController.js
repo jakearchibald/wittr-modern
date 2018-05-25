45c8f39e9b8ac370db9238834c86b6c4901ebd7b
Jake Archibald <jaffathecake@gmail.com>
2018-05-25 08:25:03 +0100

    Modernising some JS

--- public/js/main/IndexController.js
+++ public/js/main/IndexController.js
@@ -2,66 +2,67 @@ import PostsView from './views/Posts';
 import ToastsView from './views/Toasts';
 import idb from 'idb';
 
-export default function IndexController(container) {
-  this._container = container;
-  this._postsView = new PostsView(this._container);
-  this._toastsView = new ToastsView(this._container);
-  this._lostConnectionToast = null;
-  this._openSocket();
-  this._registerServiceWorker();
-}
-
-IndexController.prototype._registerServiceWorker = function() {
-  // TODO: register service worker
-};
-
-// open a connection to the server for live updates
-IndexController.prototype._openSocket = function() {
-  var indexController = this;
-  var latestPostDate = this._postsView.getLatestPostDate();
-
-  // create a url pointing to /updates with the ws protocol
-  var socketUrl = new URL('/updates', window.location);
-  socketUrl.protocol = 'ws';
-
-  if (latestPostDate) {
-    socketUrl.search = 'since=' + latestPostDate.valueOf();
+export default class IndexController {
+  constructor (container) {
+    this._container = container;
+    this._postsView = new PostsView(this._container);
+    this._toastsView = new ToastsView(this._container);
+    this._lostConnectionToast = null;
+    this._openSocket();
+    this._registerServiceWorker();
   }
 
-  // this is a little hack for the settings page's tests,
-  // it isn't needed for Wittr
-  socketUrl.search += '&' + location.search.slice(1);
+  _registerServiceWorker() {
+    // TODO: register service worker
+  }
 
-  var ws = new WebSocket(socketUrl.href);
+  // open a connection to the server for live updates
+  _openSocket() {
+    const latestPostDate = this._postsView.getLatestPostDate();
 
-  // add listeners
-  ws.addEventListener('open', function() {
-    if (indexController._lostConnectionToast) {
-      indexController._lostConnectionToast.hide();
+    // create a url pointing to /updates with the ws protocol
+    const socketUrl = new URL('/updates', window.location);
+    socketUrl.protocol = 'ws';
+
+    if (latestPostDate) {
+      socketUrl.search = 'since=' + latestPostDate.valueOf();
     }
-  });
 
-  ws.addEventListener('message', function(event) {
-    requestAnimationFrame(function() {
-      indexController._onSocketMessage(event.data);
+    // this is a little hack for the settings page's tests,
+    // it isn't needed for Wittr
+    socketUrl.search += '&' + location.search.slice(1);
+
+    const ws = new WebSocket(socketUrl.href);
+
+    // add listeners
+    ws.addEventListener('open', () => {
+      if (this._lostConnectionToast) {
+        this._lostConnectionToast.hide();
+      }
     });
-  });
 
-  ws.addEventListener('close', function() {
-    // tell the user
-    if (!indexController._lostConnectionToast) {
-      indexController._lostConnectionToast = indexController._toastsView.show("Unable to connect. Retrying…");
-    }
+    ws.addEventListener('message', event => {
+      requestAnimationFrame(() => {
+        this._onSocketMessage(event.data);
+      });
+    });
 
-    // try and reconnect in 5 seconds
-    setTimeout(function() {
-      indexController._openSocket();
-    }, 5000);
-  });
-};
+    ws.addEventListener('close', () => {
+      // tell the user
+      if (!this._lostConnectionToast) {
+        this._lostConnectionToast = this._toastsView.show("Unable to connect. Retrying…");
+      }
 
-// called when the web socket sends message data
-IndexController.prototype._onSocketMessage = function(data) {
-  var messages = JSON.parse(data);
-  this._postsView.addPosts(messages);
-};
+      // try and reconnect in 5 seconds
+      setTimeout(() => {
+        this._openSocket();
+      }, 5000);
+    });
+  }
+
+  // called when the web socket sends message data
+  _onSocketMessage(data) {
+    const messages = JSON.parse(data);
+    this._postsView.addPosts(messages);
+  }
+}
--- public/js/sw/index.js
+++
@@ -1,5 +0,0 @@
-import './preroll/index.js';
-
-self.addEventListener('fetch', function(event) {
-  console.log(event.request);
-});
